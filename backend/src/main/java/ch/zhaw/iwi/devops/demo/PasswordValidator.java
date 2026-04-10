6b0cdb8468a1fc73dfdca4531db8e77cbd9dcf47
zilancavas <cavaszil@students.zhaw.ch>
2026-04-10 18:39:23 +0200

    LN06: Lint, Selenium, SonarQube Quality

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/PasswordValidator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/PasswordValidator.java
@@ -1,13 +1,12 @@
 package ch.zhaw.iwi.devops.demo;
-
 public class PasswordValidator {
-
     private static final int MIN_LENGTH = 8;
-
     public boolean validate(String password) {
         boolean isLongEnough = password.length() >= MIN_LENGTH;
         boolean hasUpperCase = password.matches(".*[A-Z].*");
         return isLongEnough && hasUpperCase;
     }
-
+    public boolean hasNumber(String password) {
+        return password.matches(".*\\d.*");
+    }
 }
--- backend/src/test/java/ch/zhaw/iwi/devops/demo/PasswordValidatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/PasswordValidatorTest.java
@@ -1,19 +1,26 @@
 package ch.zhaw.iwi.devops.demo;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
 public class PasswordValidatorTest {
-
     @Test
-    public void testPasswordTooShort() {
+    void testPasswordTooShort() {
         PasswordValidator validator = new PasswordValidator();
         assertFalse(validator.validate("abc"));
     }
-@Test
-public void testPasswordNoUpperCase() {
-    PasswordValidator validator = new PasswordValidator();
-    assertFalse(validator.validate("abcdefgh"));
-}
+    @Test
+    void testPasswordNoUpperCase() {
+        PasswordValidator validator = new PasswordValidator();
+        assertFalse(validator.validate("abcdefgh"));
+    }
+    @Test
+    void testPasswordValid() {
+        PasswordValidator validator = new PasswordValidator();
+        assertTrue(validator.validate("Abcdefgh"));
+    }
+    @Test
+    void testPasswordHasNumber() {
+        PasswordValidator validator = new PasswordValidator();
+        assertTrue(validator.hasNumber("abc123"));
+    }
 }
